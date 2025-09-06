c92bc9fc5041282f9190cfae8b5a6df4e7a33472
Randeepa Ariyawansa <148050843+Randeepa23@users.noreply.github.com>
2025-09-06 12:38:11 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,4 +1,18 @@
-"""Subpackage containing all of pip's command line interface related code
-"""
+from typing import List, Optional
 
-# This file intentionally does not import submodules
+from pip._internal.utils import _log
+
+# init_logging() must be called before any call to logging.getLogger()
+# which happens at import of most modules.
+_log.init_logging()
+
+
+def main(args: Optional[List[str]] = None) -> int:
+    """This is preserved for old console scripts that may still be referencing
+    it.
+
+    For additional details, see https://github.com/pypa/pip/issues/7498.
+    """
+    from pip._internal.utils.entrypoints import _wrapper
+
+    return _wrapper(args)
